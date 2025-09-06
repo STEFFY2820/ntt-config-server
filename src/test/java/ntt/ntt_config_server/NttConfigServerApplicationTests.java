b1ec71b344f5c1c5647c230e8250fdf4de58b976
Fabian216 <fabian.alan.olaya@gmail.com>
2025-09-06 12:54:35 -0500

    first commit

---
+++ src/test/java/ntt/ntt_config_server/NttConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ntt.ntt_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NttConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
